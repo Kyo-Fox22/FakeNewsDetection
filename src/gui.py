28a02287dda5ae72d3c6da57580ccf34c355ca57
Kyo-Fox22 <kayle.gamundoy@gmail.com>
2026-06-15 12:22:31 +0800

    removed pyqt6 gui tests from notebook and added more widgets to gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,24 +1,47 @@
-from PyQt6.QtWidgets import QApplication,  QMainWindow, QWidget, QLabel, QTextEdit, QVBoxLayout
+from PyQt6.QtWidgets import (QApplication,  QMainWindow, QWidget, QLabel, 
+                             QLineEdit, QTextEdit, QPushButton, QVBoxLayout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.setWindowTitle('Fake News Detector App')
+        self.setWindowTitle('Detect Fake News')
             
         # Create Layout
         layout = QVBoxLayout()
         
-        textlabel = QLabel('<h1>Enter Author: </h1>')
-        textbox = QTextEdit()
+        self.authlabel = QLabel('<h4>Enter Author:</h4>')
+        self.authbox = QLineEdit()
+        self.authbox.setFixedSize(200,30)
         
-        layout.addWidget(textlabel)
-        layout.addWidget(textbox)
+        self.contentlabel = QLabel('<h4>Enter Content:</h4>')
+        self.contentbox = QTextEdit()
+        self.contentbox.setFixedSize(250,200)
         
-        # Create Window
+        self.predictbutton = QPushButton('Make Prediction')
+        self.predictbutton.clicked.connect(self.placeholder_func)
+        self.predictionbox = QLabel('<h4>Output:</h4>No prediction yet.')
+        # If fake: The given news is likely fake news.
+        # If not fake: The given news is likely authentic news.
+        
+        layout.addWidget(self.authlabel)
+        layout.addWidget(self.authbox)
+        layout.addWidget(self.contentlabel)
+        layout.addWidget(self.contentbox)
+        layout.addWidget(self.predictbutton)
+        layout.addWidget(self.predictionbox)
+        
+        # Create Main Window
         window = QWidget()
         window.setLayout(layout)
         self.setCentralWidget(window)
+        self.setFixedSize(270,380)
+        
+    def placeholder_func(self):
+        author = self.authbox.text()
+        content = self.contentbox.toPlainText()
+        print(f'{author}: {content}')
+        return
     
         
 app = QApplication([])
